5f9ad1109fb7cdb30e1836f3daf9c77ce07acfd0
hechao <formblackt@gmail.com>
2015-11-29 08:04:32 +0000

    var

---
+++ basic.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+# comment
+'''comment
+'''
+spam = True
+spam2 = False
+spam3 = ("aaa")
+a = 11
+b = 2
+
+print (spam)
+print (spam2)
+print (spam3)
+print (a)
+print (b)
+print (a+b)
+print (a-b)
+print (a*b)
+print(a/b)
+print a**b
+print a%b # modulo
---
+++ var.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+# anything
+monty = True
+python = 1.234
+monty_python = python**2
+
+print monty_python
