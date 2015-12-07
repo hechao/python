1d2034038956eb3b63ef2a797ea4bbb75d7e9fd3
root <root@localhost>
2015-12-08 00:13:17 +0800

    update

---
+++ web/dict_learn.py
@@ -0,0 +1,14 @@
+d ={}
+a = 'abc'
+c = 33
+
+d[a] =c
+
+e = 'fds'
+f = 998
+
+d[e]=f
+print d
+
+for key in d:
+	print key, d[key]
--- web/jisi.py
+++ web/jisi.py
@@ -1,36 +1,52 @@
 #! /usr/bin/python
 
 from bs4 import BeautifulSoup
+from operator import itemgetter
 import requests
 import urllib2
 
-url = 'http://www.jisilu.cn/data/bond/\#tlink_1'
+url = 'http://www.jisilu.cn/data/bond/?do_search=&sort_column=&sort_order=&forall=1&from_rating_cd=A&from_issuer_rating_cd=A&from_year_left=0&from_repo=0&from_ytm=4&from_volume=0&from_market=&y1=&y2=&to_rating_cd=AAA&to_issuer_rating_cd=AAA&to_year_left=25&to_repo=2&to_ytm=30&to_volume='
 
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, from_encoding="utf8")
 
-def pr_title():
-	print soup.title
+def find_value():
 
-def find_tbody(y):	
-	tbody_all = y.find_all('tbody')[0]
-	return tbody_all
+	d = {}
 
-def find_tr(body):
-	tr_all = body.find_all('tr')
-	return tr_all
+	tbody = soup.find_all('tbody')[0]
+	tr_list = tbody.find_all('tr')
+	print "avaialbe number is    " + str(len(tr_list))
 
-tr_input = raw_input("enter: ")
+	for x in range(0, len(tr_list),1):
 
-tbody_result = find_tbody(soup)
-tr_result = find_tr(tbody_result)
+		name = tr_list[x].find_all('td')[0].find_all('a')[0].get('href')
+		name = name[18:]
+		ytm  = tr_list[x].find_all('td')[10].get('ytm')
 
-result =  tr_result[int(tr_input)]
-print result
+		if len(ytm) !=0:
+			value_ytm = round(float(ytm),1)
+			#print value_ytm
+			d[name] = value_ytm
+		else:
+			value_ytm = 0
+			#print "ytm is 0"
+			d[name] = value_ytm
+	
+	#d2 = sorted(d.iteritems(), key=itemgetter(1), reverse=True)
+	return d
 
-log = open('log.html', 'w')
-log.writelines(str(result))
-log.close()
+result = find_value()
+#print result
 
+for key in result:
+	if result[key] >8:
+		print key, result[key]
 
+def write_log(result):
+	log = open('log.html', 'w')
+	log.writelines(str(result))
+	log.close()
+
+write_log(result)
 
