387e7c7976e81e34400adb0a6fe9b1a542d35f6d
root <root@localhost>
2015-12-07 18:33:13 +0800

    del

--- pratice/web/web-requests.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/python
-import requests
-
-url = raw_input("paste your url:   ")
-resp = requests.get('http://'+url)
-print resp.text
--- pratice/web/web-test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-
-import urllib2
-
-url = raw_input("paste the url:   ")    
-req = urllib2.Request(url)    
-response = urllib2.urlopen(req)    
-the_page = response.read()    
-print the_page  
