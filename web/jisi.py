9f4f271597119544c38b711de257e9e0ce9842c2
root <root@localhost>
2015-12-07 20:42:46 +0800

    first attempt

--- web/jisi.py
+++ web/jisi.py
@@ -8,8 +8,27 @@ url = 'http://www.jisilu.cn/data/bond/\#tlink_1'
 response = requests.get(url)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
-print soup.title
-body = soup.find_all('tbody')[0]
-#print body
-tr = body.find_all('tr')
-print tr[1]
+def pr_title():
+	print soup.title
+
+def find_tbody(y):	
+	tbody_all = y.find_all('tbody')[0]
+	return tbody_all
+
+def find_tr(body):
+	tr_all = body.find_all('tr')
+	return tr_all
+
+tr_input = raw_input("enter: ")
+
+tbody_result = find_tbody(soup)
+tr_result = find_tr(tbody_result)
+
+result =  tr_result[int(tr_input)]
+print result
+
+log = open('log.html', 'w')
+log.writelines(str(result))
+log.close()
+
+
