b9b9483ff1ff5966d7b4d716e481a191e948efb4
root <root@python>
2015-12-08 11:42:05 -0500

    V1.1

--- proj-jisi/jisi.py
+++ proj-jisi/jisi.py
@@ -4,15 +4,23 @@ from bs4 import BeautifulSoup
 import urllib2
 import datetime
 import os
+from readuser import read_user
+
+user = read_user(0)
+
+user_seq = user['1'][0]
+user_name = user['1'][1]
+user_profit = user['1'][2]
+user_email = user['1'][3]
+
+print "current setup is for %s, number is %s, profit target %s, email is %s" % (user_name, user_seq, user_profit, user_email)
+#print type(user_email)
 
 url = 'http://www.jisilu.cn/data/bond/?do_search=&sort_column=&sort_order=&forall=1&from_rating_cd=A&from_issuer_rating_cd=A&from_year_left=0&from_repo=0&from_ytm=4&from_volume=0&from_market=&y1=&y2=&to_rating_cd=AAA&to_issuer_rating_cd=AAA&to_year_left=25&to_repo=2&to_ytm=30&to_volume='
 
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, from_encoding="utf8")
 
-tred =8.5
-
-#tred = input ("please enter a check value for bond profit:  ")
 
 def find_value():
 
@@ -52,7 +60,7 @@ def high_d(result_raw):
 	high_d={}
 
 	for key in result_raw:
-		if result_raw[key] > tred:
+		if result_raw[key] > float(user_profit):
 
 			high_d[key] = result_raw[key]
 			line_data = "Bond name: %s, Bond profit: %s;" % (key, result_raw[key]) + "\n"
@@ -66,8 +74,8 @@ result = high_d(result_raw)
 
 def send(result):
 	if len(result) !=0:
-		os.system("mail -s 'Find matched bond!!' formblackt@gmail.com < log.html")
+		os.system("mail -s 'Find matched bond for you (%s) !!' %s < log.html" % (user_name, user_email))
+	else:
+		print "None find!"
 
 send(result)
-
-
--- proj-jisi/readuser.py
+++ proj-jisi/readuser.py
@@ -1,15 +1,20 @@
-f = open('/root/python/proj-jisi/user.txt')
-d = f.readlines()
-f.close()
-mydict ={}
 
-for usr in d:
-	usr_num = usr.split(':')[0]
-	usr_name = usr.split(':')[1]
-	usr_target = usr.split(':')[2]
-	usr_intv = usr.split(':')[3].rstrip('\n')
-	mydict[usr_num] = (usr_num, usr_name, usr_target, usr_intv)
-	print mydict
 
-for x in mydict:
-	print "This user's number is %s, user name is %s, user target profit is %s, user interval is %s)" % (x, mydict[x][1], mydict[x][2],mydict[x][3])
+def read_user(user_seq):
+	f = open('/root/python/proj-jisi/user.txt')
+	d = f.readlines()
+	f.close()
+	mydict ={}
+
+	for usr in d:
+		usr_num = usr.split(':')[0]
+		usr_name = usr.split(':')[1]
+		usr_target = usr.split(':')[2]
+		usr_intv = usr.split(':')[3].rstrip('\n')
+		mydict[usr_num] = (usr_num, usr_name, usr_target, usr_intv)
+		#print mydict
+
+	"""for x in mydict:
+		print "This user's number is %s, user name is %s, user target profit is %s, user interval is %s)" % (x, mydict[x][1], mydict[x][2],mydict[x][3])"""
+
+	return mydict
