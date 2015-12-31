36b663009b6091f04c091b741d90e741a358b5d0
root <root@localhost>
2015-12-31 17:48:35 +0800

    del extra in cookie_get

--- jisi2/cookie_get.py
+++ jisi2/cookie_get.py
@@ -1,21 +1,12 @@
 #! /usr/bin/python
 #-*- encoding: utf-8 -*-
 
-from bs4 import BeautifulSoup
-from urllib2 import urlopen, Request
-import json
-import cookielib
-#import sys
-#reload(sys)  
-#sys.setdefaultencoding('utf8')
-
 import cookielib, urllib2
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
 # default User-Agent ('Python-urllib/2.6') will *not* work
-
 opener.addheaders = [
     ('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11'),
     ]
