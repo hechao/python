94fc1f1380f887bd17a316eb15c90a521e8e1ad6
root <root@localhost>
2015-12-31 17:41:56 +0800

    stage..

--- jisi2/cookie_get.py
+++ jisi2/cookie_get.py
@@ -14,12 +14,11 @@ import cookielib, urllib2
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 # default User-Agent ('Python-urllib/2.6') will *not* work
-opener.addheaders = [
+"""opener.addheaders = [
     ('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11'),
     ]
 
-
-"""stylesheets = [
+stylesheets = [
     'https://www.idcourts.us/repository/css/id_style.css',
     'https://www.idcourts.us/repository/css/id_print.css',
 ]"""
--- practice/t.py
+++ practice/t.py
@@ -1,11 +1,14 @@
-def median(list):
-    if len(list) %2 == 0:
-        k = list[len(list)/2]
-    elif len(list) == 1:
-        k = list[0]
-    else:
-        list = list.sorted(list)
-        k = (float(list[len(list)/2])+float(list[len(list)/2+1]))/2
-    return k
+#-*- encoding: utf-8 -*-
+import urllib2
+headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
 
-median([4, 5, 5, 4])
+req = urllib2.Request(url = 'http://www.xueqiu.com',headers = headers)
+
+feeddata = urllib2.urlopen(req).read()
+
+#或者
+#opener = urllib2.build_opener()
+
+#feeddata = opener.open(request).read()
+
+print feeddata.decode('u8')
