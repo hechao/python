569e5712d939ccab91f6a4d08fbc20f1da0c2b56
root <root@localhost>
2015-12-31 17:43:25 +0800

    cookie get

--- jisi2/cookie_get.py
+++ jisi2/cookie_get.py
@@ -13,15 +13,12 @@ import cookielib, urllib2
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
 # default User-Agent ('Python-urllib/2.6') will *not* work
-"""opener.addheaders = [
+
+opener.addheaders = [
     ('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11'),
     ]
-
-stylesheets = [
-    'https://www.idcourts.us/repository/css/id_style.css',
-    'https://www.idcourts.us/repository/css/id_print.css',
-]"""
 
 home = opener.open('https://www.xueqiu.com')
 print cj
