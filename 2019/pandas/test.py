8c5191a7f27a0e32660603a8375eaf6c46ac6959
root <info@electrodragon.com>
2019-09-03 07:55:58 -0400

    update

--- 2019/pandas/test.py
+++ 2019/pandas/test.py
@@ -1,20 +1,41 @@
 import pandas as pd
 import numpy as np
 
-#df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
-
 df =  pd.DataFrame(np.random.randn(5, 3), columns=list('ABC'))
 
-html = (df.style.format(percent).render())
+
+def highlight_cols(s):
+     color = 'grey'
+     return 'background-color: %s' % color
+
+	 
+def color_negative_red(val):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    color = 'red' if val < 0 else 'black'
+    return 'color: %s' % color
+	
+df2 = df.style.applymap(color_negative_red)
+
+#df.style.applymap(highlight_cols, subset=pd.IndexSlice[:, ['B', 'C']])
+df2.to_html('index.html')
+
+
+
+#data.style
+
+
+
+#html = (df.style.format(percent).render())
+
+
+
+#df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
 
 #print(data)
-# def highlight_cols(s):
-    # color = 'grey'
-    # return 'background-color: %s' % color
-
-#data.style.applymap(highlight_cols, subset=pd.IndexSlice[:, ['B', 'C']])
-
-data.style
 
 #print(data)
 
